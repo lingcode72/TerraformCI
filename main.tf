c24cc7128da675900b625d6de7180297ae215c50
lingcode72 <ling.curty.simplon@gmail.com>
2023-07-04 16:47:30 +0200

    gyutru

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "random_string" "uniquestring" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "ling-terraform-cloud2"
+  name     = "ling-terraform-cloud"
   location = "francecentral"
 }
 
